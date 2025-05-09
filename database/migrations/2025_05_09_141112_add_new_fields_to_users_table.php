1ff7ce63849157b479387dffe23cc9b2d28ccaa4
Prabodha Harshani <160100195+Laayaa94@users.noreply.github.com>
2025-05-09 19:49:27 +0530

    customeAuth Controller

--- database/migrations/2025_05_09_141112_add_new_fields_to_users_table.php
+++ database/migrations/2025_05_09_141112_add_new_fields_to_users_table.php
@@ -14,7 +14,7 @@ class AddNewFieldsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+             $table->enum('type', ['Admin','User']);
         });
     }
 
